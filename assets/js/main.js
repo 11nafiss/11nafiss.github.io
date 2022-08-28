71514e4ed28b8f84e6b2d95ad087336b92a773c6
11nafiss <m.nafis.2001@gmail.com>
2022-08-28 19:50:25 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,21 @@
-const menuToggle = document.querySelector(".menu-toggle input");
-const ul = document.querySelector(".ul-res");
-
-menuToggle.addEventListener("click", function () {
-  ul.classList.toggle("slide");
-});
-
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
-});
-
 $(".owl-carousel").owlCarousel({
-  center: true,
-  items: 1,
-  loop: true,
-  stagePadding: 300,
-  autoplay: true,
-  autoWidth: true,
-  autoHeight: true,
-  margin: 10,
-  nav: true,
-  navText: ["<img src='./assets/img/Left button.svg'/>", "<img src='./assets/img/Right button.svg'/>"],
-  dots: false,
-  responsive: {
-    600: {
-      items: 2,
+    center: true,
+    loop: true,
+    autoplay: true,
+    autoWidth: true,
+    autoHeight: true,
+    margin: 10,
+    nav: true,
+    navText: ["<button type='button' class='carousel_prev'><i class='bi bi-chevron-left'></i></button>", 
+            "<button type='button' class='carousel_next'><i class='bi bi-chevron-right'></i></button>"],
+    dots: false,
+    responsive: {
+    0: {
+        items: 1
+        },
+    768: {
+        items: 1,
+        stagePadding: 50
+        },
     },
-  },
 });
